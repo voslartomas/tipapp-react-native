a336092ace56a78eaae50829f6111c0e44b4d5cd
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-05-01 21:02:51 +0200

    added

--- src/drawer.js
+++ src/drawer.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import {DrawerNavigator} from 'react-navigation';
 import ScreenComponent from './components/screen.component';
 import MainComponent from './components/main.component';
+import LeagueService from './services/league.service';
 
-export default (logout) => {
+export default async (logout) => {
   // load leagues from API /api/leagues/active
-  const leagues = [{id: 1, name: 'NHL 18'}, {id: 2, name: 'MS Rusko 18'}]
+  const leagues = await LeagueService.getLeagues();
 
   const items = {}
   leagues.forEach(league => {
