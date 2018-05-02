4840ab2032412650297b32e61e07dfeb994855a4
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-05-02 13:30:12 +0200

    selector

--- src/components/main.component.js
+++ src/components/main.component.js
@@ -62,39 +62,41 @@ export default class MainComponent extends Component {
   async componentDidMount() {
     const matches = await MatchService.getBetMatches(1)
     this.setState({ matches });
+    await this.loadBets()
 
     if (this.props.redirectLogout) {
       this.props.logout()
     }
-    await this.loadBets()
   }
 
   render() {
     console.log(this.state.matches)
+    console.log(this.state.players)
     return (
       <View style={styles.container}>
-      <ScrollView>
-      <Text style={{textAlign: 'center', color: 'white'}}>`Liga cislo: {this.props.leagueId}`</Text>
-        {this.state.matches.map(match => (
-          <Card title={match.homeTeam + " : " +  match.awayTeam}>
-            <Text>{match.matchHomeScore}:{match.matchAwayScore}{match.matchOvertime ? 'P' : ''}</Text>
-            <Text>Datum: {moment(new Date(match.matchDateTime)).fromNow()}</Text>
-            {this.canBet(match) &&
-              <TextInput value={match.homeScore || 0} type="number" name="homeScore" min="0" style={{ width: '35px' }} onChange={e => this.handleBetChange(match, e)} /> + ":" + 
-              <TextInput value={match.awayScore || 0} type="number" name="awayScore" min="0" style={{ width: '35px' }} onChange={e => this.handleBetChange(match, e)} />
-            }
-            
-            {/*this.canBet(match) && 
-              <Picker>
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
+        <ScrollView>
+        {/*<Text style={{textAlign: 'center', color: 'white'}}>`Liga cislo: {this.props.leagueId}`</Text>*/}
+          {this.state.matches.map(match => (
+            <Card title={match.homeTeam + " : " +  match.awayTeam}>
+              <Text>{match.matchHomeScore}:{match.matchAwayScore}{match.matchOvertime ? 'P' : ''}</Text>
+              <Text>Datum: {moment(new Date(match.matchDateTime)).fromNow()}</Text>
+              {this.canBet(match) &&
+                (<TextInput value={match.homeScore || 0} type="number" name="homeScore" min="0" style={{ width: '35px' }} onChange={e => this.handleBetChange(match, e)} /> 
+                <Text>:</Text>
+                <TextInput value={match.awayScore || 0} type="number" name="awayScore" min="0" style={{ width: '35px' }} onChange={e => this.handleBetChange(match, e)} />
+                )}
+              <Picker onValueChange={(e, { name, value }) => {this.handleBetChange(match, e, value)}} selectedValue={match.scorerId}>
+                {
+                  this.getPlayers(match).map(player => (
+                    <Picker.Item label={player.name} value={player.id} />
+                  ))
+
+                }
               </Picker>
-            */}
-          </Card>
-        ))}
+            </Card>
+          ))}
         </ScrollView>
         <Button onPress={this.props.logout} title="odhlasit se"></Button>
-        
       </View>
     );
   }
