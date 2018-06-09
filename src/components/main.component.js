6e29bd6c173ac234806385cf9e2bcd2e0c8979b9
Tomas Voslar <tomas.voslar@iflix.com>
2018-06-09 08:21:18 +0200

    Adjustments.

--- src/components/main.component.js
+++ src/components/main.component.js
@@ -3,8 +3,24 @@ import LeaderboardComponent from './dashboard/leaderboard.component';
 import DashboardComponent from './dashboard/dashboard.component'
 import BetsMatchComponent from './dashboard/bets.match.component';
 import BetsSerieComponent from './dashboard/bets.serie.component';
+import styles from '../styles'
 
 import { TabNavigator } from 'react-navigation';
+
+const dashboardOptions = {
+  lazy: true,
+  tabBarPosition: 'top',
+  tabBarOptions: {
+    activeTintColor: styles.secondary,
+    inactiveTintColor: 'white',
+    style: {
+      backgroundColor: styles.primary,
+      color: 'black',
+    },
+  },
+}
+const mainOptions = Object.assign({}, dashboardOptions)
+mainOptions.tabBarPosition = 'bottom'
 
 export default (league) => {
   return TabNavigator({
@@ -12,7 +28,7 @@ export default (league) => {
       screen: TabNavigator({
         Matches: {screen: props => <BetsMatchComponent leagueId={league.leagueId} />},
         Series: {screen: props => <BetsSerieComponent leagueId={league.leagueId}/>}
-    })},
+    }, dashboardOptions)},
     Leadeboard: {screen: props => <LeaderboardComponent leagueId={league.leagueId} />},
-  })
+  }, mainOptions)
 }
