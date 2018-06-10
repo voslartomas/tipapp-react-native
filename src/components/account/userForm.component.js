1580c2ff27bfd278173cb2c4f460e50e10c80f95
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-06-10 20:59:39 +0200

    profile edit styles added

--- src/components/account/userForm.component.js
+++ src/components/account/userForm.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, ScrollView, Dimensions, Button, Alert, TextInput, Text } from 'react-native';
 import UserService from '../../services/user.service'
+import styles from '../../styles'
 
 export default class UserFormComponent extends Component {
   constructor(props) {
@@ -24,24 +25,32 @@ export default class UserFormComponent extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Upravit profil</Text>
+      <View style={styles.container}>
+        <Text style={styles.normalText}>Upravit profil</Text>
         <TextInput
+          style={styles.input}
+          placeholderTextColor='white'
           placeholder="Křestní jméno"
           value={this.state.user.firstName}
           onChangeText={text => this.setState({ user: { ...this.state.user, firstName: text } })}
         />
         <TextInput
+          style={styles.input}
+          placeholderTextColor='white'
           placeholder="Příjmení"
           value={this.state.user.lastName}
           onChangeText={text => this.setState({ user: { ...this.state.user, lastName: text } })}
         />
         <TextInput
+          style={styles.input}
+          placeholderTextColor='white'
           placeholder="E-Mail"
           value={this.state.user.email}
           onChangeText={text => this.setState({ user: { ...this.state.user, email: text } })}
         />
         <TextInput
+          style={styles.input}
+          placeholderTextColor='white'
           placeholder="Telefon"
           value={this.state.user.mobileNumber}
           onChangeText={text => this.setState({ user: { ...this.state.user, mobileNumber: text } })}
